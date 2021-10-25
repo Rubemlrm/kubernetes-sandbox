2847024c3081ab98d905fbd1757480561eca22cc
Rubem Mota <rubemmota89@gmail.com>
2021-10-25 09:26:02 +0100

    changed from minikube to kind cluster services

--- setup.sh
+++ setup.sh
@@ -4,4 +4,12 @@ helm repo add grafana https://grafana.github.io/helm-charts
 
 echo "Installing needed charts"
 helm install prometheus prometheus-community/prometheus
+helm install prometheus-exporter prometheus-community/prometheus-node-exporter
 helm install grafana grafana/grafana
+
+echo "Setup services"
+kubectl create -f grafana/
+kubectl create -f hello-deployment/
+kubectl create -f prometheus/
+kubectl create -f syncthing
+kubectl create -f ingress-controller.yaml
