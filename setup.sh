a7c5de725d34c66b8ae08771e67314b9ffefb36a
Rubem Mota <rubemmota89@gmail.com>
2021-10-26 11:15:55 +0100

    Added prometheus-helm-stack and ingress controller to handle this helm chart

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo add grafana https://grafana.github.io/helm-charts
 
 echo "Installing needed charts"
-#helm install prometheus prometheus-community/prometheus --create-namespace
-#helm install prometheus-exporter prometheus-community/prometheus-node-exporter --create-namespace
-helm install monitoring prometheus-community/kube-prometheus-stack --create-namespace
+kubectl create namespace syncthing
+kubectl create namespace hello-deployment
+kubectl create namespace monitoring
+helm install monitoring prometheus-community/kube-prometheus-stack --namespace monitoring
 
 echo "Setup services"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-kubectl create -f grafana/
-kubectl create -f hello-deployment/
-kubectl create -f prometheus/
+sleep 40
+kubectl create -f hello-deployment
 kubectl create -f syncthing
-kubectl create -f ingress-controller.yaml
+kubectl create -f monitoring
